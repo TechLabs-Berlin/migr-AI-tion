6c2bec82fd8234bba52b941e55c09357672c789a
paul <65470117+datapumpernickel@users.noreply.github.com>
2020-12-18 18:35:19 +0100

    updating requirements

--- backend/src/router/images.py
+++ backend/src/router/images.py
@@ -5,10 +5,11 @@ from sqlalchemy.orm import Session
 # import dependencies
 from fastapi import APIRouter, Depends,File, UploadFile
 
+# this is for creating image ids
 from uuid import uuid4
 
+#this is to read and save images
 from PIL import Image as pil_image
-from io import BytesIO
 
 from database.database import get_session
 from models.images import Image
@@ -22,44 +23,31 @@ router = APIRouter()
 # D = DELETE -> @router.delete("/{id}")
 # L = LIST -> @router.get("")
 
-# @router.post("")
-# def create_image(image: Dict, session: Session = Depends(get_session)) -> Image:
-#   """Create a new image.
-
-#   Args:
-#       image (Dict): Image dictionary including name, uri and author.
-#       session (Session, optional): SQLAlchemy Session. Defaults to Depends(get_session).
-
-#   Returns:
-#       Image: A database image instance.
-#   """
-#   # create a new image instance
-#   db_image = Image(**image)
-#   # register image in session
-#   session.add(db_image)
-#   # save changes in database
-#   session.commit()
-#   # reload image from database
-#   session.refresh(db_image)
-#   return db_image
-
 @router.post("")
 async def create_image(file: UploadFile = File(...), session: Session = Depends(get_session)) -> Image:
     """[summary]
 
     Args:
-        file (UploadFile, optional): [description]. Defaults to File(...).
-        session (Session, optional): [description]. Defaults to Depends(get_session).
+        file (UploadFile, optional): [description]. Accepts file objects, Pillow is powerful, 
+        so probably accepts a bunch of image types, but for now onlz jpg and png
+
+        session (Session, optional): Gets session object for database connection. Defaults to Depends(get_session).
 
     Returns:
-        Image: [description]
+        Image: Image database instance. 
+                Saves image to ./images
     """
+    #check if it is jpeg or other format we like
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
+    #create uuid, so that it can use it for filename
     uuid = str(uuid4().hex)
+    #open the image and make pillow-image object
     im = pil_image.open(file.file)
+    #save pillow image object
     im.save("./images/"+uuid, "JPEG")
+    #create database dictionary with the necessary information
     image_dict = {"id": uuid, "name": file.filename, "uri":"./images/"+uuid }
     # create a new image instance
     db_image = Image(**image_dict)
@@ -70,9 +58,6 @@ async def create_image(file: UploadFile = File(...), session: Session = Depends(
     # reload image from database
     session.refresh(db_image)
     return db_image
-
-
-
 
 @router.get("")
 def list_images(session: Session = Depends(get_session)) -> List[Image]:
@@ -87,11 +72,11 @@ def list_images(session: Session = Depends(get_session)) -> List[Image]:
   return session.query(Image).all()
 
 @router.get("/{id_}")
-def read_image(id_: int, session: Session = Depends(get_session)) -> Image:
+def read_image(id_: str, session: Session = Depends(get_session)) -> Image:
   """[summary]
 
   Args:
-      id_ (int): The id of the image to return.
+      id_ (str): The id of the image to return.
       session (Session, optional): SQLAlchemy Session. Defaults to Depends(get_session).
 
   Returns:
