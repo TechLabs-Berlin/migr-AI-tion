344687eb8a0359d453584dbbbba7fd681f7eeffd
YOUR GITHUB HANDLE <YOUR GITHUB EMAIL>
2020-12-21 18:47:53 +0100

    created button and image display api call
    not yet connected

---
+++ frontend/src/Uploader.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Uploader(props) {
+  const [upload, setUpload] = useState({ ready: false });
+
+  function displayImageUpload(response) {
+    setUpload({
+      ready: true,
+      image: response.images.name,
+    });
+  }
+  function handleImageUpload(response) {
+    console.log(response.images);
+    let imageID = "response.images.name";
+    axios.get(imageID).then(displayImageUpload);
+  }
+
+  function handleUploadSubmit(event) {
+    event.preventDefault();
+    let apiKey = "";
+    let apiURL = ``;
+    axios.post(apiURL).then(handleImageUpload);
+  }
+
+  let uploadButton = (
+    <div className="button">
+      <button onClick={handleUploadSubmit}>+ Upload!</button>
+    </div>
+  );
+
+  if (ready) {
+    return (
+      <div>
+        <Display img={props.images.name} />
+        {uploadButton}
+      </div>
+    );
+  } else {
+    return uploadButton;
+  }
+}
