211b9bd272ca4704e63a47db6d02461012675817
YOUR GITHUB HANDLE <YOUR GITHUB EMAIL>
2020-12-28 21:42:55 +0100

    Began integrating react-upload-gallery with customRequest

--- frontend/src/Uploader/UploadGallery.js
+++ frontend/src/Uploader/UploadGallery.js
@@ -1,8 +1,48 @@
-import React from "react";
-import RUG from "react-upload-gallery";
+import React from 'react';
+import RUG, { DragArea, DropArea, Card, List } from 'react-upload-gallery';
+import axios from 'axios'; 
 
 export default function UploadGallery(event) {
-  return (
+
+    customRequest ({ uid, file, send, action, headers, onProgress, onSuccess, onError }) {
+        const form = new FormData();
+        form.append('file', file)
+        const CancelToken = axios.CancelToken
+        const source = CancelToken.source()       
+
+        axios.post(
+            action,
+            form,
+            {
+                headers: {
+                    'x-access-token' : 'xxxx'
+                },
+                onUploadProgress: ({ total, loaded }) => {
+                    onProgress(uid, Math.round(loaded / total * 100));
+                },
+                cancelToken: source.token
+            }
+        ).then(({ data: response }) => {
+            onSuccess(uid, response);
+        })
+        .catch(error => {
+            onError(uid, {
+                action,
+                status: error.request,
+                response: error.response
+            })
+        })
+
+      //having trouble with class extends Components vs. function components: where do these returns go?
+         
+        return {
+            abort() {
+                source.cancel()
+            }
+        }
+    }
+
+  return(
     <RUG
       action="/api/upload"
       source={(response) => response.source}
@@ -11,9 +51,8 @@ export default function UploadGallery(event) {
         size: 20,
         width: { min: 1280, max: 1920 },
         height: { min: 720, max: 1080 },
-    
-    accept={['jpg', 'jpeg', 'png']}, 
-      }}
+        accept={['jpg', 'jpeg', 'png']}, 
+        customRequest={customRequest}
     />
-  );
+  )
 }
