c59abff6a18cc4ef921dab517e88849d01b15ca6
YOUR GITHUB HANDLE <YOUR GITHUB EMAIL>
2021-01-12 02:58:15 +0100

    Added NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import ProjectIntroPage from "./ProjectIntroPage";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <ProjectIntroPage />
     </div>
   );
