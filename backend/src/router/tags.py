decfcc33ea014e2930fa108f319eaa2ba228f89b
paul <65470117+datapumpernickel@users.noreply.github.com>
2021-01-25 21:51:00 +0100

    fixed typo

--- backend/src/router/tags.py
+++ backend/src/router/tags.py
@@ -34,23 +34,13 @@ def list_tags(counts: bool = False, session: Session = Depends(get_session)) -> 
     Returns:
         List[Tag]: [description]
     """
-    if counts is False:
-        # initialize tag contoller
-        tag_controller = TagController(session=session)
-        return tag_controller.list()
-    else:
-        result = session.execute('SELECT a.tag_id "source", b.tag_id "target", COUNT(*) "value" FROM images_tags a	JOIN images_tags b ON b.image_id = a.image_id	AND b.tag_id > a.tag_id	GROUP BY a.tag_id,		b.tag_id')
-        links = []
-        for row in result:
-            links.append(row)
-        result = session.execute('SELECT ALL images_tags.tag_id id,t.tag "name",COUNT(*) "value" FROM images_tags LEFT JOIN tags t ON images_tags.tag_id=t.id GROUP BY images_tags.tag_id')
-        nodes = []
-        for row in result:
-            nodes.append(row)
-        return {"nodes": nodes, "links": links}
+    # initialize tag contoller
+    tag_controller = TagController(session=session)
+    return tag_controller.list()
+
 
 @router.get("/network")
-def get_network(session: Session = Depends(get_session)) -> List[Tag]:
+def get_network(session: Session = Depends(get_session)) :
     """[summary]
 
     Args:
