828924dde372bdd520d39ab3f00b3953062a49b6
YOUR GITHUB HANDLE <YOUR GITHUB EMAIL>
2021-01-26 16:06:56 +0100

    rewrote project intro and finished styling

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -11,21 +11,28 @@ export default function Project() {
       <Grid item xs={11} md={8} lg={6}>
         <section className="project-intro-text">
           <h3>
-            -aims to raise more awareness about marginalization and data
-            literacy, and to educate the public about hidden AI bias and
-            invisible labor through a platform for collaboration and interactive
-            data visualization. The goal of this art, documentary, and tech
-            project is to reimagine the construction of AI technologies as
-            participatory processes to heal historical ruptures and collective
-            traumas through new forms of storytelling. The proposal is to build
-            a platform to collaborate with migrants on an image dataset to train
-            future computer vision technologies. The app will include an
-            interactive data visualization of computer vision word index to
-            which users can contribute. The initial sample data of at least 100
-            words and 300 images will be created in collaboration and
-            consultation with migrants, refugees, experts, and activists. This
-            will give migrants a chance to participate in the development of new
-            technologies and the future media landscape.
+            <u>
+              <b>migr-AI-tion</b>
+            </u>
+            <br />
+            is an art, documentary and tech project inspired by new
+            possibilities of <mark>storytelling</mark> through data. We focus on
+            migration issues and representing <mark> migrant voices</mark> that
+            are not heard in the mainstream media. Our methods are collaborative
+            and participatory, and we aim to bring more public awareness to
+            issues such as{" "}
+            <i>
+              <b> data literacy</b>
+            </i>
+            ,{" "}
+            <i>
+              <b>invisible labour</b>
+            </i>{" "}
+            and {""}
+            <i>
+              <b>data colonialism</b>
+            </i>
+            .
           </h3>
         </section>
         <UploadDialog></UploadDialog>
