9068cfc1ea6588142a59968c9fc0f12218215bd0
YOUR GITHUB HANDLE <YOUR GITHUB EMAIL>
2021-01-29 22:25:51 +0100

    created echarts

--- frontend/src/components/chart/Chart.js
+++ frontend/src/components/chart/Chart.js
@@ -1,26 +1,62 @@
 import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
+import ReactEcharts from "echarts-for-react";
 import * as echarts from "echarts";
 import axios from "axios";
 
-function Chart({ options }) {
-  var myChart = echarts.init(useRef("null"));
+function Chart({ option }) {
+  var myChart = echarts.init(useRef(null));
+  var chart = myChart.current;
 
   myChart.showLoading();
-  useEffect(() => {
-    const chart = myChart.current;
-    chart.setOption(options);
-  }, [options, ResizeObserver]);
 
-  axios.get("http://127.0.0.1:8000/tags/network", function (graph) {
+  useEffect(() => {
+    chart.setOption(option);
+  }, [option, chart]);
+
+  axios.get("http://127.0.0.1:8000/tags/network").then((graph) => {
     myChart.hideLoading();
-    graph.nodes.forEach(function (node) {
+    graph.nodes.forEach((node) => {
       node.symbolSize = 5;
     });
 
-    return <div ref={myChart} style={{ width: "100%", height: "100%" }}></div>;
+    return (
+      <ReactEcharts
+        ref={myChart}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        option={{
+          title: {
+            text: "Les Miserables",
+            subtext: "Default layout",
+            top: "bottom",
+            left: "right",
+          },
+          tooltip: {},
+          series: [
+            {
+              name: "Les Miserables",
+              type: "graph",
+              layout: "force",
+              data: graph.nodes,
+              links: graph.links,
+              roam: true,
+              label: {
+                position: "right",
+              },
+              force: {
+                repulsion: 100,
+              },
+            },
+          ],
+        }}
+      />
+    );
   });
-  Chart.propTypes = { options: PropTypes.any };
 }
+
+Chart.propTypes = { option: PropTypes.any };
 
 export default Chart;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Chart from "../components/chart/Chart";
-import network from "../components/chart/network";
 
 export default function Home() {
   return (
     <div>
-      <Chart options={network} />
+      <Chart />
     </div>
   );
 }
