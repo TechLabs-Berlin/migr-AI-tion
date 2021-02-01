451d2b76b55853d9d3166c89d5bbcc9d93785f40
paul <65470117+datapumpernickel@users.noreply.github.com>
2021-02-01 18:40:15 +0100

    added response model for network tags

--- backend/src/router/tags.py
+++ backend/src/router/tags.py
@@ -1,6 +1,7 @@
 # import type hints
 from typing import List
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, aliased
+from sqlalchemy import func
 
 # import dependencies
 import os
@@ -10,7 +11,9 @@ from uuid import uuid4  # this is for creating image ids
 from uuid import UUID
 from database.database import get_session
 from models.tags import Tag
+from models.images_tags import ImageTag
 from controllers.tags import TagController
+from views.responses.tags import node, link, ReadNetwork
 
 
 
@@ -39,8 +42,8 @@ def list_tags(counts: bool = False, session: Session = Depends(get_session)) -> 
     return tag_controller.list()
 
 
-@router.get("/network")
-def get_network(session: Session = Depends(get_session)) :
+@router.get("/network", response_model=ReadNetwork)
+def get_network(session: Session = Depends(get_session)) -> ReadNetwork :
     """[summary]
 
     Args:
@@ -50,13 +53,12 @@ def get_network(session: Session = Depends(get_session)) :
         List[Tag]: [description]
     """
 
-    result = session.execute('SELECT a.tag_id "source", b.tag_id "target", COUNT(*) "value" FROM images_tags a	JOIN images_tags b ON b.image_id = a.image_id	AND b.tag_id > a.tag_id	GROUP BY a.tag_id,		b.tag_id')
-    links = []
-    for row in result:
-        links.append(row)
-    result = session.execute('SELECT ALL images_tags.tag_id id,t.tag "name",COUNT(*) "value" FROM images_tags LEFT JOIN tags t ON images_tags.tag_id=t.id GROUP BY images_tags.tag_id')
-    nodes = []
-    for row in result:
-        nodes.append(row)
-    return {"nodes": nodes, "links": links}
+    nodes = session.query(ImageTag.tag_id.label("id"), func.count(ImageTag.tag_id).label("value"), Tag.tag.label("name")).group_by(ImageTag.tag_id).join(Tag).all()
+    
+    a = aliased(ImageTag)
+    b = aliased(ImageTag)
+    links = session.query(a.tag_id.label("source"), b.tag_id.label("target"), func.count('*').label("value")).join(b, b.image_id==a.image_id).filter(a.tag_id > b.tag_id).group_by(a.tag_id, b.tag_id).all()
+
+
+    return {"links": links, "nodes": nodes}
 
--- backend/src/views/responses/tags.py
+++ backend/src/views/responses/tags.py
@@ -1,6 +1,7 @@
 # import dependencies
-from pydantic import BaseModel
+from pydantic import BaseModel,validator
 from uuid import UUID
+from typing import List
 
 
 class ReadTag(BaseModel):
@@ -9,6 +10,42 @@ class ReadTag(BaseModel):
     """
     id: UUID
     tag: str
+
+    class Config:
+        orm_mode = True
+
+
+class node(BaseModel):
+    id: UUID
+    name: str
+    value: float
+
+    @validator("id")
+    def process_uuid_to_hex(cls, v):
+        return v.hex
+
+    class Config:
+        orm_mode = True
+
+
+class link(BaseModel):
+    source: UUID
+    target: UUID
+    value: float
+
+    @validator("source", "target")
+    def process_uuid_to_hex(cls, v):
+        return v.hex
+
+    class Config:
+        orm_mode = True
+
+class ReadNetwork(BaseModel):
+    """
+    The view of an annotation response.
+    """
+    nodes: List[node]
+    links: List[link]
 
     class Config:
         orm_mode = True
