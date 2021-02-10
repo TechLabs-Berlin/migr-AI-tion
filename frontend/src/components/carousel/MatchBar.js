80e05e23b6ac69f1441dd8eb6cd9a9c24a2eebe6
YOUR GITHUB HANDLE <YOUR GITHUB EMAIL>
2021-02-10 17:21:11 +0100

    finished confidence bar

--- frontend/src/components/carousel/MatchBar.js
+++ frontend/src/components/carousel/MatchBar.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Typography from "@material-ui/core/Typography";
 
 const BorderLinearProgress = withStyles((theme) => ({
-    root: {
-        height: 13,
-        borderRadius: 5,
-        marginLeft: 7,
-        marginRight: 9,
-    },
-    colorPrimary: {
-        backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
-    },
-    bar: {
-        borderRadius: 5,
-        backgroundColor: '#668389',
-    },
+  root: {
+    height: 13,
+    borderRadius: 5,
+    marginLeft: 7,
+    marginRight: 9,
+  },
+  colorPrimary: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
+  },
+  bar: {
+    borderRadius: 5,
+    backgroundColor: "#668389",
+  },
 }))(LinearProgress);
 
-
 const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1,
+  },
 });
 
 export default function MatchBar({ match, aitag }) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className="matchbar-wrapper">
-            <BorderLinearProgress className={classes.root} variant="determinate" value={match} />
-            <Typography variant="body2">{`${match}% ${aitag}`}</Typography>
-        </div>
-    );
+  return (
+    <div className="matchbar-wrapper">
+      <BorderLinearProgress
+        className={classes.root}
+        variant="determinate"
+        value={match}
+      />
+      <Typography variant="body2">
+        {match >= 9 ? `${match.toFixed(1)}% ${aitag}` : `${match}% ${aitag}`}
+      </Typography>
+    </div>
+  );
 }
--- frontend/src/components/carousel/SearchBar2.js
+++ frontend/src/components/carousel/SearchBar2.js
@@ -1,198 +1,217 @@
-import { Typography, Chip, Avatar, InputBase } from '@material-ui/core';
-import React, { useState, useEffect } from 'react'
+import { Typography, Chip, Avatar, InputBase } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AiOutlineNumber } from "react-icons/ai";
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import './SearchBar2.css';
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import "./SearchBar2.css";
 import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import Slider from 'react-slick';
-import MatchBar from './MatchBar';
-import ImageModal from './ImageModal';
+import Slider from "react-slick";
+import MatchBar from "./MatchBar";
+import ImageModal from "./ImageModal";
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: "#9611FF",
-        },
-        secondary: {
-            main: "#668389",
-        },
+  palette: {
+    primary: {
+      main: "#9611FF",
     },
+    secondary: {
+      main: "#668389",
+    },
+  },
 });
 
-
 export default function Searchbar2() {
+  {
+    /*This is the const for Searchbar */
+  }
 
-    {/*This is the const for Searchbar */ }
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
-    const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('');
-    const [results, setResults] = useState([]);
+  function onSubmit(e) {
+    e.preventDefault();
+    setQuery(search);
+    console.log(search);
+  }
 
-    function onSubmit(e) {
-        e.preventDefault();
-        setQuery(search);
-        console.log(search);
+  function onSearch(e) {
+    setSearch(e.target.value);
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `http://127.0.0.1:8000/images?tag=${query}`
+        );
+        const json = await response.json();
+        console.log({ json });
+        setResults(json);
+      } catch (error) {}
     }
 
-    function onSearch(e) {
-        setSearch(e.target.value);
+    if (query !== "") {
+      console.log("the data does not exist");
+      fetchData();
     }
+  }, [query]);
 
+  {
+    /*This is the const for Carousel */
+  }
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch(`http://127.0.0.1:8000/images?tag=${query}`);
-                const json = await response.json();
-                console.log({ json });
-                setResults(
-                    json
-                )
-            } catch (error) { }
-        }
+  const settings = {
+    dots: true,
+    className: "center",
+    infinite: true,
+    slidesToShow: 1,
+    speed: 500,
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-        if (query !== '') {
-            console.log("the data does not exist")
-            fetchData();
-        }
-    }, [query])
+  {
+    /*this is the searchbar return part*/
+  }
 
-    {/*This is the const for Carousel */ }
+  return (
+    <div className="all">
+      <div className="paperwrapper-searchbar">
+        <form component="form" onSubmit={onSubmit} className="searchbar-paper">
+          <IconButton
+            type="submit"
+            aria-label="search"
+            style={{ paddingRight: "0.3em", paddingLeft: "0.5em" }}
+          >
+            <SearchIcon className="search-icon" />
+          </IconButton>
+          <InputBase
+            value={search}
+            onChange={onSearch}
+            placeholder="Search tags.."
+            variant="outlined"
+            className="searchbar-input"
+            style={{ fontSize: "0.9em" }}
+          />
+        </form>
+      </div>
 
-    const settings = {
-        dots: true,
-        className: "center",
-        infinite: true,
-        slidesToShow: 1,
-        speed: 500,
-        initialSlide: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
+      {/*This is the Carousel part*/}
 
+      <div>
+        <ThemeProvider theme={theme}>
+          <Slider {...settings} className="slickslider">
+            {results.map((item) => (
+              <div>
+                <div key={item.id} className="paper-slider">
+                  <div className="img-wrapper">
+                    <ImageModal identifier={item.id} />
+                  </div>
+                  <div>
+                    <Typography
+                      variant="h6"
+                      component="p"
+                      className="caption-wrap"
+                    >
+                      <i>"{item.caption}"</i>
+                    </Typography>
+                  </div>
 
-    {/*this is the searchbar return part*/ }
+                  <div className="mtag-wrap">
+                    <Typography className="mtag-label">
+                      migr-AI-tion Tags:
+                    </Typography>
+                    {item.tags.map((posttag) => {
+                      console.log(posttag.tag);
+                      return (
+                        <Chip
+                          className="chip1"
+                          avatar={
+                            <Avatar>
+                              <AiOutlineNumber />
+                            </Avatar>
+                          }
+                          key={posttag.id}
+                          label={posttag.tag}
+                          component="a"
+                          href="#chip"
+                          variant="outlined"
+                          color="primary"
+                          clickable
+                        />
+                      );
+                    })}
+                  </div>
+                  <div className="ntag-wrap">
+                    <Typography className="ntag-label">
+                      Image_Net Tags:
+                    </Typography>
 
-    return (
-        <div className="all">
-            <div className="paperwrapper-searchbar">
-                <form component="form" onSubmit={onSubmit} className="searchbar-paper">
-                    <IconButton
-                        type="submit"
-                        aria-label="search"
-                        style={{ paddingRight: "0.3em", paddingLeft: "0.5em" }}>
-                        <SearchIcon className="search-icon" />
-                    </IconButton>
-                    <InputBase
-                        value={search}
-                        onChange={onSearch}
-                        placeholder="Search tags.."
-                        variant="outlined"
-                        className="searchbar-input"
-                        style={{ fontSize: "0.9em" }}
-                    />
-                </form>
-            </div>
-
-            {/*This is the Carousel part*/}
-
-            <div>
-                <ThemeProvider theme={theme}>
-                    <Slider {...settings} className="slickslider">
-                        {results.map((item) => (
-                            <div>
-                                <div key={item.id} className="paper-slider">
-                                    <div className="img-wrapper">
-                                        <ImageModal identifier={item.id} />
-                                    </div>
-                                    <div>
-                                        <Typography variant="h6" component="p" className="caption-wrap">
-                                            <i>"{item.caption}"</i>
-                                        </Typography>
-                                    </div>
-
-                                    <div className="mtag-wrap">
-                                        <Typography className="mtag-label">
-                                            migr-AI-tion Tags:
-                                            </Typography>
-                                        {item.tags.map(posttag => {
-                                            console.log(posttag.tag);
-                                            return (
-                                                <Chip className="chip1" avatar={<Avatar><AiOutlineNumber /></Avatar>} key={posttag.id} label={posttag.tag} component="a" href="#chip" variant="outlined" color="primary" clickable />
-                                            )
-                                        })}
-                                    </div>
-                                    <div className="ntag-wrap">
-                                        <Typography className="ntag-label">
-                                            Image_Net Tags:</Typography>
-
-                                        {/* this is a pre-code for ai_tags
-                                        {item.ai_tags.map(posttag => {
-                                            console.log(posttag.tag);
-                                            return (
-                                                <Chip className="chip2" style={{ color: "#668389" }} avatar={<Avatar style={{ background: "#668389" }}><AiOutlineNumber style={{ color: "white" }} /></Avatar>} key={posttag.id} label={posttag.tag} component="a" href="#chip" clickable />
-                                            )
-                                        })}
-                                        */}
-
-                                        {item.tags.map(posttag => {
-                                            console.log(posttag.tag);
-                                            return (
-                                                <Chip className="chip2" style={{ color: "#668389" }} avatar={<Avatar style={{ background: "#668389" }}><AiOutlineNumber style={{ color: "white" }} /></Avatar>} key={posttag.id} label={posttag.tag} component="a" href="#chip" clickable />
-                                            )
-                                        })}
-                                    </div>
-                                    <div>
-                                        <MatchBar match={parseInt("75 %")} aitag={'tradition'} />
-                                        <MatchBar match={parseInt("40 %")} aitag={'tradition'} />
-                                        <MatchBar match={parseInt("60.5 %")} aitag={'tradition'} />
-
-                                        {/*  this is a pre-code for MatchBar     
-                                        {item.ai_tags.map(aiitem => {
-                                            return (
-                                                <MatchBar match={parseInt(aiitem.confidence)} aitag={aiitem.tag}>
-                                            )
-                                        })}
-                                        */}
-
-                                    </div>
-
-
-                                </div>
-                            </div>)
-                        )}
-                    </Slider>
-                </ThemeProvider>
-            </div>
-
-        </div>
-    );
+                    {item.ai_tags.map((posttag) => {
+                      console.log(posttag.tag);
+                      return (
+                        <Chip
+                          className="chip2"
+                          style={{ color: "#668389" }}
+                          avatar={
+                            <Avatar style={{ background: "#668389" }}>
+                              <AiOutlineNumber style={{ color: "white" }} />
+                            </Avatar>
+                          }
+                          key={posttag.id}
+                          label={posttag.tag}
+                          component="a"
+                          href="#chip"
+                          clickable
+                        />
+                      );
+                    })}
+                  </div>
+                  <div>
+                    {item.ai_tags.map((aiitem) => {
+                      return (
+                        <MatchBar
+                          match={parseFloat(aiitem.confidence)}
+                          aitag={aiitem.tag}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
 }
